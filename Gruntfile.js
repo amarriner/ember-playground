225a04865cc4ce87540cd769e8c1b92e1d56ef89
amarriner <amarriner@amarriner.com>
2015-10-06 14:35:39 -0400

    Added npm http-server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,21 @@
 module.exports = function(grunt) {
   grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: 'src'
+        }
+      }
+    }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
